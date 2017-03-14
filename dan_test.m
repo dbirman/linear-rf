b7bb2164b25b8ccec73d301db54583b16c6bb784
Dan Birman <danbirman@gmail.com>
2017-03-13 23:23:52 -0700

    Pushing updates for forward attention model

--- dan_test.m
+++ dan_test.m
@@ -15,7 +15,7 @@ addpath(genpath(pwd))
 % receptive fields)
 
 %% Load params file
-load(fullfile(datafolder_lrf,'pRFparams.mat'));
+load(fullfile(datafolder_lrf,'pRFparams_concat.mat'));
 pRF.ROIs = pRF.roiNames;
 
 %% Fix structures
@@ -58,8 +58,8 @@ for ri = 1:length(pRF.ROIs)
     axis([-40 40 -20 20]);
     set(gca,'Units','Inches');
     set(gca,'Position',[1 1 5 2.5]);
-    markerWidth = diff(xlim)*axpos(3); % Calculate Marker width in points
-    scatter(croiparams(1,:),croiparams(2,:),(2*72*5/80*croiparams(3,:)).^2,'ok');
+    axpos = get(gca,'Position');
+    scatter(croiparams(1,:),croiparams(2,:),(2*72*axpos(3)/80*croiparams(3,:)).^2,'ok');
     title(pRF.ROIs{ri});
     xlabel('X (degs)');
     ylabel('Y (degs)');
@@ -94,12 +94,15 @@ end
 
 %% Use voxel mapping to constrain lasso
 
+% figure
 for mi = 1:length(mappings)
     cmap = map{mi}; % mapping: incl/excl voxels
     lower = pRF.(mappings{mi}{1});
     higher = pRF.(mappings{mi}{2});
     disppercent(-1/size(higher.tSeries,2));
-    for vi = 1:size(higher.tSeries,2)
+    highers.weights = cell(1,size(higher.tSeries,1));
+    voxels = cell(1,size(higher.tSeries,1));
+    for vi = 1:size(higher.tSeries,1)
         % for each higher voxel, compute lasso weights
         roi_data = lower.tSeries(logical(cmap(vi,:)),:);
         timecourse = higher.tSeries(vi,:);
@@ -107,11 +110,70 @@ for mi = 1:length(mappings)
         
             w = prf_lasso(roi_data,timecourse);
             higher.weights{vi} = w;
+            voxels{vi} = find(cmap(vi,:));
+            % test figure
+%             clf; hold on
+%             plot(timecourse,'b');
+%             plot(w*roi_data,'r');
+%             pause(0.001);
         end
         disppercent(vi/size(higher.tSeries,2));
     end
     disppercent(inf);
-    pRF.(mappings{mi}{2}).weights = weights;
+    pRF.(mappings{mi}{2}).weights = higher.weights;
+    pRF.(mappings{mi}{2}).lower_voxels = voxels;
+    pRF.(mappings{mi}{2}).mapping = cmap;
 end
 
+%% Save file
+fname = fullfile(datafolder_lrf,'pRFparams_map.mat');
+save(fname,'pRF');
+%% Load file
+fname = fullfile(datafolder_lrf,'pRFparams_map.mat');
+load(fname);
+
 %% Draw a figure taking a random v2 voxel and showing its underlying weights
+
+%% Check that prefit refits match existing pRF 
+
+%% Compute forward attention model
+lower = pRF.lV1;
+higher = pRF.lV2;
+
+gauss.x = 5;
+gauss.y = 5;
+gauss.sd = 3;
+
+gain_param = 1; % stronger = more powerful signal
+
+% compute the gain model
+lower.gain = ones(size(lower.linearScanCoords));
+
+for vi = 1:size(lower.tSeries,1)
+    dx = abs(gauss.x-pRF.rfParams(1,lower.linearScanCoords(vi)));
+    dy = abs(gauss.y-pRF.rfParams(2,lower.linearScanCoords(vi)));
+    dist = hypot(dx,dy);
+    lower.gain(vi) = lower.gain(vi) + normpdf(dist,0,gauss.sd) * gain_param;
+end
+
+% compute the gain tSeries
+lower.tSeries_gain = lower.tSeries .* repmat(lower.gain',1,size(lower.tSeries,2));
+
+higher.tSeries_gain = zeros(size(higher.tSeries));
+for vi = 1:size(higher.tSeries,1)
+    % V1 tSeries for this voxel
+    orig_tSeries = higher.tSeries(vi,:);
+    % which voxels in V1 to use
+    map = find(higher.mapping(vi,:));
+    if ~isempty(map)
+        higher.tSeries_gain(vi,:) = higher.weights{vi}*lower.tSeries_gain(map,:);
+    end
+end
+
+pRF.lV1 = lower;
+pRF.lV2 = higher;
+
+%% Save forward
+fname = fullfile(datafolder_lrf,'pRFparams_forward.mat');
+save(fname,'pRF');
+%% Fit pRFs 
