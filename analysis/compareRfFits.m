ab97bcafca9c74b65a29de5e1e6b56cd880e427e
Akshay Jagadeesh <akjags@gmail.com>
2017-04-19 11:55:50 -0700

    Adding RF Fits comparison script

--- analysis/compareRfFits.m
+++ analysis/compareRfFits.m
@@ -1,76 +1,96 @@
 %% compareRfFits
 %
-% Gets model response for held-out dataset using original parameters and the forward pass parameters.
+%               by: akshay jagadeesh
+%             date: 03/30/2017
+%          purpose: Gets model response for held-out dataset using original parameters and the forward pass parameters.
 %
-function cv = compareRfFits(cv, skip)
-
-
-roi = 'v2';
-lower = 'v1';
+%            input: 
+%            usage: 
+%                   load('~/Box Sync/LINEAR_RF/crossval_params.mat');
+%                   CV = compareRfFits(CV, 'lV2', 'lV1');
+%
+%                   %If modelResponse is already computed in CV
+%                   compareRfFits(CV,[],[],1);
+%
+function cv = compareRfFits(cv, lower, upper, plotFigs)
 
 folds = fields(cv); folds = folds(2:end);
 
-if ieNotDefined('skip')
-v = newView;
-v = viewSet(v, 'curGroup', 'Concatenation');
-v = viewSet(v, 'curScan', 2);
-v = loadAnalysis(v, 'pRFAnal/pRF.mat');
-concatInfo = viewGet(v, 'concatInfo');
-d = viewGet(v, 'd');
-analysisParams = viewGet(v, 'analysisParams');
+if ieNotDefined('plotFigs')
+  plotFigs=0;
+else
+  plotFigs=1;
+end
 
-v2 = loadROITSeries(v, 'lV2', [],[],'straightXform=1', 'loadType=none');
+if plotFigs==0
+  v = newView;
+  v = viewSet(v, 'curGroup', 'Concatenation');
+  v = viewSet(v, 'curScan', 2);
+  v = loadAnalysis(v, 'pRFAnal/pRF.mat');
+  concatInfo = viewGet(v, 'concatInfo');
+  d = viewGet(v, 'd');
+  analysisParams = viewGet(v, 'analysisParams');
+  analysisParams.pRFFit.verbose = 0;
 
-for i = 1:length(folds)
+  v2 = loadROITSeries(v, upper, [],[],'straightXform=1', 'loadType=none');
 
-  paramsFromStim = cv.(folds{i}).(roi).rfParams;
-  paramsForward = cv.(folds{i}).(roi).(lower).paramsForward;
+  for i = 1:length(folds)
 
-  testTSeries = cv.(folds{i}).(roi).test;
+    paramsFromStim = cv.(folds{i}).(upper).rfParams;
+    paramsForward = cv.(folds{i}).(upper).(lower).paramsForward;
 
-  modelResp_fromstim = nan(size(testTSeries)); r2_fromstim = zeros(1,size(testTSeries,1));
-  modelResp_forward = nan(size(testTSeries)); r2_forward = zeros(1,size(testTSeries,1));
-  disp(sprintf('Fold %i - Computing %d model responses using both forward and fromstim parameters', i, size(testTSeries,1)));
-  parfor vox = 1:size(testTSeries,1)
+    testTSeries = cv.(folds{i}).(upper).test;
 
-    x = v2.scanCoords(1,i); y = v2.scanCoords(2,i); z = v2.scanCoords(3,i);
-    fit = pRFFit(v, [], x,y,z, 'tSeries', testTSeries(vox,:).', 'stim', d.stim, 'getModelResponse=1', 'params', paramsFromStim(vox,:),...
-                 'concatInfo', concatInfo, 'fitTypeParams', analysisParams.pRFFit, 'paramsInfo', d.paramsInfo);
+    modelResp_fromstim = nan(size(testTSeries)); r2_fromstim = zeros(1,size(testTSeries,1));
+    modelResp_forward = nan(size(testTSeries)); r2_forward = zeros(1,size(testTSeries,1));
+    disp(sprintf('Fold %i - Computing %d model responses using both forward and fromstim parameters', i, size(testTSeries,1)));
+    tic
+    parfor vox = 1:size(testTSeries,1)
 
-    modelResp_fromstim(vox,:) = fit.modelResponse';
-    r2_fromstim(vox) = corr(fit.modelResponse, fit.tSeries)^2;
+      x = v2.scanCoords(1,i); y = v2.scanCoords(2,i); z = v2.scanCoords(3,i);
+      fit = pRFFit(v, [], x,y,z, 'tSeries', testTSeries(vox,:).', 'stim', d.stim, 'getModelResponse=1', 'params', paramsFromStim(vox,:),...
+                   'concatInfo', concatInfo, 'fitTypeParams', analysisParams.pRFFit, 'paramsInfo', d.paramsInfo);
+
+      modelResp_fromstim(vox,:) = fit.modelResponse';
+      r2_fromstim(vox) = corr(fit.modelResponse, fit.tSeries)^2;
 
 
-    fit2 = pRFFit(v, [], x,y,z, 'tSeries', testTSeries(vox,:).', 'stim', d.stim, 'getModelResponse=1', 'params', paramsForward(vox,:),...
-                  'concatInfo', concatInfo, 'fitTypeParams', analysisParams.pRFFit, 'paramsInfo', d.paramsInfo);
+      fit2 = pRFFit(v, [], x,y,z, 'tSeries', testTSeries(vox,:).', 'stim', d.stim, 'getModelResponse=1', 'params', paramsForward(vox,:),...
+                    'concatInfo', concatInfo, 'fitTypeParams', analysisParams.pRFFit, 'paramsInfo', d.paramsInfo);
 
-    modelResp_forward(vox,:) = fit2.modelResponse';
-    r2_forward(vox) = corr(fit2.modelResponse, fit2.tSeries)^2;
+      modelResp_forward(vox,:) = fit2.modelResponse';
+      r2_forward(vox) = corr(fit2.modelResponse, fit2.tSeries)^2;
 
+    end
+
+    toc
+    cv.(folds{i}).(upper).(lower).testModelRespForward = modelResp_forward;
+    cv.(folds{i}).(upper).(lower).testModelRespFromStim = modelResp_fromstim;
+    cv.(folds{i}).(upper).(lower).testR2Forward = r2_forward;
+    cv.(folds{i}).(upper).(lower).testR2FromStim = r2_fromstim;
   end
 
-  cv.(folds{i}).(roi).testModelRespForward = modelResp_forward;
-  cv.(folds{i}).(roi).testModelRespFromStim = modelResp_fromstim;
-  cv.(folds{i}).(roi).testR2Forward = r2_forward;
-  cv.(folds{i}).(roi).testR2FromStim = r2_fromstim;
+else
+  disp('Model response passed in. Skipping computing of model response');
+  allR2_diff = [];
+  allR2_forward = [];
+  allR2_fromstim = [];
+  for i = 1:length(folds)
+    r2_forward = cv.(folds{i}).(upper).(lower).testR2Forward;
+    r2_fromstim = cv.(folds{i}).(upper).(lower).testR2FromStim;
+
+    r2_diff = r2_forward - r2_fromstim;
+
+    allR2_diff = [allR2_diff; r2_diff];
+    allR2_forward = [allR2_forward; r2_forward];
+    allR2_fromstim = [allR2_fromstim; r2_fromstim];
+  end
+
+  figure;
+  plot(nanmean(allR2_forward), nanmean(allR2_fromstim), '*');
+  hold on; plot([0:.1:.8], [0:.1:.8], '-k');
+  title(sprintf('Model Comparison: Forward (%s --> %s) vs From Stim', lower, upper));
+  xlabel('Forward model cross-validated r^2');
+  ylabel('From stimulus model cross-validated r^2');
+  drawPublishAxis;
 end
-
-end
-
-allR2 = [];
-for i = 1:length(folds)
-  r2_forward = cv.(folds{i}).(roi).testR2Forward;
-  r2_fromstim = cv.(folds{i}).(roi).testR2FromStim;
-
-  r2_diff = r2_forward - r2_fromstim;
-
-  allR2 = [allR2; r2_diff];
-
-end
-meanR2 = mean(allR2,1);
-stdR2 = std(allR2,1);
-figure; errorbar(meanR2, 1.96*stdR2/2, 'o')
-title('r2_{forward}-r2_{fromStim} averaged across folds');
-xlabel('Voxel'); ylabel('Mean difference (forward - fromStim) across folds');
-drawPublishAxis
-keyboard
--- run_all.m
+++ run_all.m
@@ -3,11 +3,11 @@
 % Date: Spring, 2017
 
 %% Paths
-cd ~/proj/gru
-startup
-cd ~/proj/linear-rf
-addpath(genpath(pwd))
-cd ~/Box' Sync'/LINEAR_RF/
+%cd ~/proj/gru
+%startup
+%cd ~/proj/linear-rf
+%addpath(genpath(pwd))
+%cd ~/Box' Sync'/LINEAR_RF/
 
 %% File structure
 % crossval.mat - CV folds, original pRF and timeseries
@@ -19,7 +19,7 @@ cd ~/Box' Sync'/LINEAR_RF/
 %% Plotting functions
 % Initial:
 %   plotCV - compares intra-fold timeseries, and fold vs. test timeseries
-plotCV('~/Box Sync/LINEAR_RF/crossval.mat','lV1');
+%plotCV('~/Box Sync/LINEAR_RF/crossval.mat','lV1');
 %   plotRF - draws original receptive fields
 % Weights:
 %   plotWeights - shows which RFs contribute to which voxel (somehow?)
@@ -28,16 +28,16 @@ plotCV('~/Box Sync/LINEAR_RF/crossval.mat','lV1');
 % Params:
 %   drawRFchanges(preparams,postparams) - Draws RF shift arrows
 % Model comparison:
-plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold1','lV1','lV2');
-plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold2','lV1','lV2');
-plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold3','lV1','lV2');
-plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold4','lV1','lV2');
+%plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold1','lV1','lV2');
+%plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold2','lV1','lV2');
+%plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold3','lV1','lV2');
+%plotRFChanges('~/Box Sync/LINEAR_RF/crossval_params.mat','fold4','lV1','lV2');
 %   R^2 plots?
 
 %% Split data up into 4 folds, run pRFFit on each fold to get params, and get the CV struct
-rois = {'lV1', 'lV2'};
-%CV = crossValLinRF(rois);
-%save(fullfile('~/Box Sync/LINEAR_RF/crossval.mat'), 'CV');
+% rois = {'lV1', 'lV2', 'lV3', 'lMT'};
+% CV = crossValLinRF(rois);
+% save(fullfile('~/Box Sync/LINEAR_RF/crossval.mat'), 'CV');
 
 %% Set opts
 % from the standard macaque model it should be:
@@ -51,33 +51,58 @@ rois = {'lV1', 'lV2'};
 % that's for a future version...)
 opts = {{'lV1','lV2'},{'lV1','lV3'},{'lV2','lV3'},{'lV1','lMT'},{'lV2','lMT'}};
 %% Run Weights
-load(fullfile('~/Box Sync/LINEAR_RF/crossval.mat'));
-
-for oi = 1:length(opts)
-    opt = opts{oi};
-    low = opt{1};
-    high = opt{2};
-    CV = computeLinearWeights(CV,low,high);
-    CV = addMask(CV,low,high);
-end
-
-save(fullfile('~/Box Sync/LINEAR_RF/crossval_weights.mat'),'CV');
+% load(fullfile('~/Box Sync/LINEAR_RF/crossval.mat'));
+% 
+% for oi = 1:length(opts)
+%     opt = opts{oi};
+%     low = opt{1};
+%     high = opt{2};
+%     CV = computeLinearWeights(CV,low,high);
+%     CV = addMask(CV,low,high);
+% end
+% 
+% save(fullfile('~/Box Sync/LINEAR_RF/crossval_weights.mat'),'CV');
 
 
 %% Run Forward Model
-load(fullfile('~/Box Sync/LINEAR_RF/crossval_weights.mat'));
-for oi = 1:length(opts)
-    opt = opts{oi};
-    low = opt{1};
-    high = opt{2};
-    CV = computeForwardGain(CV,low,high,0.1,5,3,3);
-    CV = computeGainOverlap(CV,high,5,3,3);
-end
-save(fullfile('~/Box Sync/LINEAR_RF/crossval_forward.mat'),'CV');
+% load(fullfile('~/Box Sync/LINEAR_RF/crossval_weights.mat'));
+% for oi = 1:length(opts)
+%     opt = opts{oi};
+%     low = opt{1};
+%     high = opt{2};
+%     CV = computeForwardGain(CV,low,high,0.1,5,3,3);
+%     CV = computeGainOverlap(CV,high,5,3,3);
+% end
+% save(fullfile('~/Box Sync/LINEAR_RF/crossval_forward.mat'),'CV');
 
 
 %% Get pRF fit params for each of the folds after running forward model
-load(fullfile('~/Box Sync/LINEAR_RF/crossval_forward.mat'));
-CV = fitCVpRF(CV, 'lV1', 'lV2');
-save(fullfile('~/Box Sync/LINEAR_RF/crossval_params.mat'), 'CV');
+% load(fullfile('~/Box Sync/LINEAR_RF/crossval_forward.mat'));
+% for oi = 1:length(opts)
+%   opt = opts{oi};
+%   low = opt{1};
+%   high = opt{2};
+%   disp(sprintf('Computing pRF for low %s and high %s', low, high));
+%   CV = fitCVpRF(CV, low, high);
+%   save(fullfile('~/Box Sync/LINEAR_RF/crossval_params.mat'), 'CV');
+% end
 
+%% Compare RF Fits
+load(fullfile('~/Box Sync/LINEAR_RF/crossval_params.mat'));
+for oi = 1:length(opts)
+  opt = opts{oi};
+  low = opt{1};
+  high = opt{2};
+  disp(sprintf('Comparing RF Fits for low %s and high %s', low, high));
+  CV = compareRfFits(CV, low, high);
+  save(fullfile('~/Box Sync/LINEAR_RF/crossval_modelComparison.mat'), 'CV');
+end
+
+%% Display RF Fits
+load(fullfile('~/Box Sync/LINEAR_RF/crossval_modelComparison.mat'), 'CV');
+for oi = 1:length(opts)
+  opt = opts{oi};
+  low = opt{1};
+  high = opt{2};
+  compareRfFits(CV, low, high, 1);
+end
